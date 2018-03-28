1d9115fd1ac9619648e4a84b14c958b0da2eedc1
Roland Willis <rolandwillisiv@gmail.com>
2018-03-28 14:43:15 -0700

    fixed ids to answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 var game = {
   score: 0,
   currentQuestion:0,
+  gameOn: false,
 
   myQuestions: [
       {
@@ -28,7 +29,7 @@ var game = {
           b: "Minding his own business, so stop asking",
           c: "Sitting in a tree",
         },
-        correctAnswer: "c"
+        correctAnswer: "b"
       }
     ],
 
@@ -40,34 +41,62 @@ var game = {
     },
 
     drawQA: function(cq) {
-      $("#question").empty();
-      $("#question").html(this.myQuestions[cq].question);
-      $("#answers").empty();
-      $("#answers").append("<p class='answer' id='ansA'>" + this.myQuestions[cq].answers.a + "</p>");
-      $("#answers").append("<p class='answer' id='ansB'>" + this.myQuestions[cq].answers.b + "</p>");
-      $("#answers").append("<p class='answer' id='ansC'>" + this.myQuestions[cq].answers.c + "</p>");
-      
-      $("#ansA").on("load", function(){
-        alert("loaded");
-      });
+      if (cq < this.myQuestions.length) {
+        $("#question").empty();
+        $("#question").html(this.myQuestions[cq].question);
+        /*$("#answers").empty();
+        $("#answers").append("<p class='answer' id='ansA'>" + this.myQuestions[cq].answers.a + "</p>");
+        $("#answers").append("<p class='answer' id='ansB'>" + this.myQuestions[cq].answers.b + "</p>");
+        $("#answers").append("<p class='answer' id='ansC'>" + this.myQuestions[cq].answers.c + "</p>");
+        */
+        $("#ansA").empty();
+        $("#ansA").html("<p class='answer'>" + this.myQuestions[cq].answers.a + "</p>");
+        $("#ansB").empty();
+        $("#ansB").html("<p class='answer'>" + this.myQuestions[cq].answers.b + "</p>");
+        $("#ansC").empty();
+        $("#ansC").html("<p class='answer'>" + this.myQuestions[cq].answers.c + "</p>");
+      }
+
     },
 
     startGame: function() {
+      gameOn = true;
       this.drawQA(this.currentQuestion);
     },
 
-    check: function() {
-      alert("2");
+    checkAns: function(ans) {
+      if(this.myQuestions[this.currentQuestion].correctAnswer === ans) {
+        alert("yay");
+        this.score++;
+      } else {
+        alert("boo");
+      }
+      this.currentQuestion++;
+      this.drawQA(this.currentQuestion);
     }
 
 }
 
 $(document).ready(function() {
   game.initiateGame();
+  
   $("#game-btn").on("click", function() {
     game.startGame();
   });
 
+  $("#ansA").on("click", function() {
+    game.checkAns("a");
+  });
+
+  $("#ansB").on("click", function() {
+    game.checkAns("b");
+  });
+
+  $("#ansC").on("click", function() {
+    game.checkAns("c");
+  });
+
 });
+
 
 
